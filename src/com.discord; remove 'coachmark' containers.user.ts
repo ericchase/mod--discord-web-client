a721acd5cb9b01e2c28b4d96a65d22268421b359
ericchase <github@eric.addymail.com>
2026-04-05 07:40:24 -0500

    can't remove this node right away, hide for now
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/com.discord; remove 'coachmark' containers.user.ts
+++ src/com.discord; remove 'coachmark' containers.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        com.discord; remove 'coachmark' containers
 // @match       https://discord.com/*
-// @version     1.0.0
+// @version     1.0.1
 // @description 2026-04-04
 // @run-at      document-start
 // @grant       none
@@ -13,8 +13,8 @@ WebPlatform_DOM_Element_Added_Observer_Class({
   selector: 'div[class*=coachmarkContainer_]',
 }).subscribe((element) => {
   if (element.parentElement?.className?.includes?.('container_')) {
-    element.parentElement.remove();
+    element.parentElement.style.display = 'none';
   } else {
-    element.remove();
+    element.style.display = 'none';
   }
 });
